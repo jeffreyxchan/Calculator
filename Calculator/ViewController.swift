347d4c7d2a19562efc738a4c79417d40f2eb38fd
Jeffrey Chan <jeffschan97@gmail.com>
2016-12-27 11:23:38 -0800

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Jeffrey Chan on 12/27/16.
+//  Copyright © 2016 Jeffrey Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        print("touchDigit")
+    }
+    
+}
+
